1b11351fa1fd8973c09552b643a45dfa2169a79b
kimkwonho <rlarnjsgh99@gmail.com>
2021-05-27 16:02:09 +0900

    final

--- simulation/simulation.c
+++ simulation/simulation.c
@@ -27,22 +27,22 @@ void enqueue(QueueType *q, element item);
 element dequeue(QueueType *q);
 
 // 시뮬레이션에 필요한 여러가지 상태 변수
-int duration=10; // 시뮬레이션 시간
-double arrival_prob=0.7; // 하나의 시간 단위에 도착하는 평균 고객의 수
+int duration=100; // 시뮬레이션 시간
+double arrival_prob=0.1; // 하나의 시간 단위에 도착하는 평균 고객의 수
 int max_serv_time=5; // 하나의 고객에 대한 최대 서비스 시간
 int clock;
 
 
 // 시뮬레이션의 결과
-int customers; // 전체고객수
-int served_customers; // 서비스받은 고객수
-int waited_time; // 고객들이 기다린 시간
+int customers = 0; // 전체고객수
+int served_customers= 0; // 서비스받은 고객수
+int waited_time = 0; // 고객들이 기다린 시간
 
 QueueType queue;
 
 int main(){
     init(&queue);
-    int service_time[NoServiceMan]; // 서비스의 alarm clock
+    int service_time[NoServiceMan] = {0,}; // 서비스의 alarm clock
     clock=0; // global clock
     while(clock < duration){
         clock++; // clock 시간 하나 경과
@@ -51,12 +51,11 @@ int main(){
         if (is_customer_arrived()) { //new customer 처리
             insert_customer(clock);
         }
-
-        for(int k=0; k++; k< NoServiceMan) { // every service man k
-            if (service_time[k] > 0){ // 여기부터 service 처리
-                service_time[k]--;}
+        for(int a=0; a< NoServiceMan; a++){ // every service man k
+            if (service_time[a] > 0){ // 여기부터 service 처리
+                service_time[a]--;}
             else {
-            service_time[k] = remove_customer();
+            service_time[a] = remove_customer();
             }
         }
     }
@@ -80,7 +79,7 @@ void insert_customer(int arrival_time){
     customer.arrival_time = arrival_time;
     customer.service_time=(int)(max_serv_time*random()) + 1;
     enqueue(&queue, customer);
-    printf("고객 %d이 %d분에 들어옵니다. 서비스시간은 %d분입니다.", customer.id, customer.arrival_time, customer.service_time);
+    printf("고객 %d이 %d분에 들어옵니다. 서비스시간은 %d분입니다.\n", customer.id, customer.arrival_time, customer.service_time);
 }
 
 // 0에서 1사이의 실수 난수 생성 함수
@@ -96,17 +95,17 @@ int remove_customer(){
     customer = dequeue(&queue);
     service_time = customer.service_time-1;
     served_customers++;
-    waited_time += clock - customer.arrival_time;
+    waited_time += (clock - customer.arrival_time);
     printf("고객 %d이 %d분에 서비스를 시작합니다. 대기시간은 %d분이었습니다.", customer.id, clock, clock - customer.arrival_time);
     return service_time;
 }
 
 // 통계치를 출력한다.
 void print_stat(){
-    printf("서비스받은 고객수 = %d", served_customers);
-    printf("전체 대기 시간 = %d분", waited_time);
-    printf("1인당 평군 대기 시간 = %f분", (double)waited_time/served_customers);
-    printf("아직 대기중인 고객수 = %d", customers-served_customers);
+    printf("서비스받은 고객수 = %d\n", served_customers);
+    printf("전체 대기 시간 = %d분\n", waited_time);
+    printf("1인당 평군 대기 시간 = %f분\n", (double)waited_time/served_customers);
+    printf("아직 대기중인 고객수 = %d\n", customers-served_customers);
 }
 
 void init(QueueType *q){
